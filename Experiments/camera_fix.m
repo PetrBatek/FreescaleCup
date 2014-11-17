64c2ed65080ecea7498332c336b5d2677d82c46c
Jiří Kuneš <jirka642@gmail.com>
2014-11-17 23:08:20 +0100

    camera gradient

--- Experiments/camera_fix.m
+++ Experiments/camera_fix.m
@@ -12,6 +12,61 @@ switch stage
     
     case 'cut'
         out = in(15:112);
+    
+    case 'smooth'
+        l = length(in);
+        out = zeros(1,l);
+        
+        out(1) = (in(1) + in(2))/2.0;
+        out(l) = (in(l-1) + in(l))/2.0;
+        for i = 2:(l-1)
+           out(i) = (in(i-1) + in(i) + in(i+1))/3.0;
+        end
+        
+    case 'gradient'
+        l = length(in);
+        in = in - min(in); % nastav minimalni hodnotu na 0
+        in_max = max(in);
+        out = zeros(1,l-1); % pole gradientu ma o jedna mensi velikost
+        
+        for i = 1:(l-1)
+            out(i) = (in(i+1) - in(i)) / in_max;
+        end
+        
+    case 'filter gradient'
+        l = length(in);
+        for i = 1:l
+            if in(i) > 0.03
+                in(i) = 1;
+            elseif in(i) < -0.03
+                in(i) = -1;
+            else
+                in(i) = 0;
+            end
+        end
+        out = in;
+        
+    case 'cut gradient' % odstrani poklesy na stranach
+        l = length(in);
+        out = in;
+        
+        % zleva
+        for i = 1:l
+            if out(i)~=0
+                out(i) = 0;
+            else
+                break;
+            end
+        end
+        
+        % zprava
+        for i = fliplr(1:l)
+            if out(i)~=0
+                out(i) = 0;
+            else
+                break;
+            end
+        end
 end
 
 end
--- Experiments/camera_plot_data.m
+++ Experiments/camera_plot_data.m
@@ -18,3 +18,33 @@ data = camera_fix(data,'cut');
 figure;
 plot(data);
 title('2. oseknuti dat');
+
+% Vyhlazeni
+data = camera_fix(data,'smooth');
+
+figure;
+plot(data);
+title('3. vyhlazeni dat');
+
+% Gradient
+gradient = camera_fix(data,'gradient');
+
+figure;
+plot(gradient);
+title('4. Gradent');
+
+% Filter gradient
+gradient = camera_fix(gradient,'filter gradient');
+
+figure;
+plot(gradient);
+title('5. Filter Gradent');
+axis([0, length(gradient), -1.2, 1.2]);
+
+% Cut gradient
+gradient = camera_fix(gradient,'cut gradient');
+
+figure;
+plot(gradient);
+title('6. Cut Gradent');
+axis([0, length(gradient), -1.2, 1.2]);
